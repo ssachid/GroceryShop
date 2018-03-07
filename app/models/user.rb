d8c23c0d220b2e635b2968c1c0ac80a025a3d586
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-07 14:38:21 -0800

    Added validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ApplicationRecord
+  # before callbacks
+  before_save { self.email = email.downcase }
+
+  # associations
   has_many :orders, dependent: :destroy
   has_many :products, through: :orders
+
+  # validations
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false},
+                       length: { minimum: 3, maximum: 15 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { maximum: 100 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
+
 end
