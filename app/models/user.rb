1963934089d2f6577ac2a80d77ec332c65fce6f8
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-07 14:39:14 -0800

    Added bcrypt has_secure_password and validations for password field

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
 
+  validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
+  has_secure_password
 end
