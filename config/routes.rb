6f313cc45e7d79ccd79703b6fac35e841aaa4095
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-07 16:06:40 -0800

    Added navbar and footer, homepage view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+  root "welcome#home"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
