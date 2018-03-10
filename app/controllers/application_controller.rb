c583409f7fd1ca021cde878271f6c21b3c86771f
Shambhavi Sachid <shambu1985@gmail.com>
2018-03-10 10:56:11 -0800

    Handled error edge cases

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
 
+  # returns the currently logged in user
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  # checks if the user is logged_in?
   def logged_in?
     !!current_user
   end
 
+  # sets the flash and redirects to root path if user not logged in
   def require_user
     if !logged_in?
       flash[:danger] = "You must be logged in to perform that action"
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,39 @@
 class ProductsController < ApplicationController
+  before_action :require_admin, only: [:sold]
+
   def index
     @products = Product.all.paginate(page: params[:page], per_page: 5)
   end
 
   def sold
     if params[:products_sold].present?
-      @start_date = params[:products_sold][:from]
-      @end_date = params[:products_sold][:to]
-      @option = params[:products_sold][:option]
-      if @start_date && @end_date
+      @start_date, @end_date, @option = params[:products_sold][:from],
+                                        params[:products_sold][:to],
+                                        params[:products_sold][:option]
+
+      if @start_date.present? && @end_date.present? && @option.present?
+        # returns all the orders created within a time range
         orders = Order.created_between(@start_date, @end_date)
-        sold_orders_by_option = sold_orders_by_option(orders, @option)
-        @sold_products = get_products_for_orders(sold_orders_by_option)
+
+        if !orders.empty?
+          # groups orders by selected option -> day/week/month
+          sold_orders_by_option = sold_orders_by_option(orders, @option)
+          # returns all the products for orders
+          @sold_products = get_products_for_orders(sold_orders_by_option)
+        else
+          flash[:danger] = "No orders for this time duration"
+          redirect_back(fallback_location: root_path)
+        end
+      else
+        flash[:danger] = "Start date, End date or Option invalid"
+        redirect_back(fallback_location: root_path)
       end
     end
-
   end
 
 
   private
+
   def sold_orders_by_option(collection, option)
     if !(collection.empty?) && !option.empty?
       if option == 'Day'
@@ -32,34 +47,36 @@ class ProductsController < ApplicationController
   end
 
   def get_products_for_orders(orders)
-    # orders = {timestamp => [o1, o2, o3...], { timestamp: [...]}}
     products = orders.reduce({}) do |result, (key,values)|
       result[key] = values.map(&:products).flatten
-      result # { timestamp: [p1,p2,p3,...], timestamp: [p1,p2,..]}
+      result
     end
     products_hash = convert_object_to_attributes(products)
-    p "*" * 50
-    # p products_hash
     combine_quantity_for_duplicate_products(products_hash)
   end
 
   def convert_object_to_attributes(products)
     products.reduce({}) do |result, (key,values)|
-      # result[key] = values.map(&:attributes)
       result[key] = values.map { |p| {"#{p.name}" => "#{p.quantity}"} }
       result
     end
   end
 
   def combine_quantity_for_duplicate_products(products)
-    # {Thu, 08 Mar 2018=>[{"Kane Bechtelar"=>"8.0"}, {"Kane Bechtelar"=>"8.0"},
-                        # {"Anastasia Considine"=>"3.0"}, {"Trace Lebsack"=>"7.0"}, ..]}
     products.reduce({}) do |result, (key, products_array)|
       products_array = products_array.inject() do |memo, product|
-        memo.merge(product) { |k, old_v, new_v| old_v.to_i + new_v.to_i}
+        memo.merge(product) { |k, old_v, new_v| old_v.to_f + new_v.to_f}
       end
       result[key] = products_array
       result
+    end
+  end
+
+  # checks if the user is an admin
+  def require_admin
+    if !logged_in? && !current_user.admin?
+      flash[:error] = "You must be an admin to perform that action"
+      redirect_back(fallback_location: root_path)
     end
   end
 end
